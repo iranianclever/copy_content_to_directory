7ad516441acdb2e69a0260c5fc055cd1d67ea155
iranianclever <iranianclever@gmail.com>
2022-12-02 12:37:05 +0330

    redesign structure of class & add_fetch, add_source functions

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
+import json
 from distutils.dir_util import copy_tree
 
 
 class CopyContentToDirectory:
     """Copy contents of a directory to another"""
 
-    def __init__(self, source, destination):
+    def __init__(self):
         """Initialization"""
-        self.source = source
-        self.destination = destination
+        self.__PATH = 'sources.json'
 
-    def copy(self):
+    def copy(self, source, destination):
         """Copy to another"""
         try:
-            copy_tree(self.source, self.destination)
+            copy_tree(source, destination)
+            return True
         except Exception as e:
             print(e)
             return False
-        finally:
+
+    def add_source(self, key, source):
+        """Play while impact command"""
+        sources = self.__fetch_sources()
+        try:
+            # Add source to sources
+            with open(self.__PATH, 'a') as f:
+                # Add last source
+                sources[key] = source
+                # Convert to json format
+                last_sources = json.dumps(sources)
+                # Write to file path
+                f.write(last_sources)
             return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def __fetch_sources(self):
+        """return all sources.json"""
+        try:
+            # Init sources
+            sources = {}
+            # Open file sources
+            with open(self.__PATH) as f:
+                # Load sources to data
+                data = f.read()
+                # Checkout data for load to json format
+                if data:
+                    sources = json.loads(data)
+            return sources
+        # If file sources not found, create file and recursive the function
+        except FileNotFoundError:
+            open(self.__PATH, 'w')
+            return self.__fetch_sources()
 
 
+# Main running...
 if __name__ == '__main__':
-    source = input('Source directory: ')
-    destination = input('Destination directory: ')
-    copy_content = CopyContentToDirectory(source, destination)
-
-    status = copy_content.copy()
-    if status:
-        print('Done!')
-    else:
-        print('Your operation broken!')
+    copy = CopyContentToDirectory()
+    copy.add_source('windows', 'c:\\user\\Desktop')
